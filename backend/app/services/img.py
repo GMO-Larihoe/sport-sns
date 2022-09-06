cb9983dc320b723f9686c0c3596d94fbe560f623
鈴木稜太朗 <srro1991@gmail.com>
2022-09-06 16:21:20 +0900

    画像の保存

--- backend/app/services/img.py
+++ backend/app/services/img.py
@@ -31,9 +31,7 @@ def save_icon_imag(
 ):
     bin_data = io.BytesIO(base64.b64decode(imag))
     re_imag = read_image(bin_data)
-    if old_path != './icon/defalut.jpg' or old_path != None:
-            os.remove(old_path)
-    path = './icon/' + str(uuid.uuid4()) + '.jpg' 
+    path = './food/' + str(uuid.uuid4()) + '.jpg' 
     cv2.imwrite(path, re_imag)
     return path
 

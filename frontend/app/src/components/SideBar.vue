54ab5ae2c26113d40072b7f92f878a703df79a72
ueda <doraemon123453@gmail.com>
2022-09-06 16:42:38 +0900

    change

---
+++ frontend/app/src/components/SideBar.vue
@@ -0,0 +1,32 @@
+<script>
+    import TheButton from './TheButton.vue';
+    export default {
+        name: 'SideBar',
+        components: 'TheButton',
+    }
+  </script>
+
+<template>
+   
+<div class="side">
+    <TheButton />
+</div>
+</template>
+
+<style scoped>
+    .side{
+        position:absolute;
+        background-color:#727272;
+        width:15%;
+        margin-left:0;
+        padding:0;
+        height:100vh;
+        /*marginss-top:100px;*/
+        text-align:center;
+        
+    }
+     /* #meaple{ 
+        position: absolute;
+        text-align:center;
+    } */
+</style>
---
+++ frontend/app/src/components/create/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="side">
+</div>
+<div class="side2">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+
+  data(){
+    return{
+      nowmsg : 'apple'
+    };
+ 
+  },
+  computed: {
+    test() {
+      switch(this.nowmsg){
+        case 'apple': 
+        case 'melon':
+        case 'orange':
+          return this.nowmsg;
+        default:
+          return 'test';
+      }
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+body{
+        font-family:'Noto Sans JP',sans-serif;
+        background-color:#f6f5f4;
+} 
+.flex{
+  display:flex;
+}
+.side{
+    position:absolute;
+    background-color:#ff838b;
+    width:15vw;
+    height: 100vh;
+    top:0vh;
+    left:0vw;
+}
+</style>

096b167e50e8f7daf652a8b9575b1fd753453db5
鈴木稜太朗 <srro1991@gmail.com>
2022-09-09 13:50:29 +0900

    初期データの更新

--- db/sql/command.sql
+++ db/sql/command.sql
@@ -1,4 +1,49 @@
 charset utf8mb4;
+insert into users values (1, 'admin', 'admin@admin.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 0);
+insert into users values (2, 'user1', 'user1@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (3, 'user2', 'user2@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (4, 'user3', 'user3@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (5, 'user4', 'user4@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (6, 'user5', 'user5@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (7, 'user6', 'user6@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (8, 'user7', 'user7@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (9, 'user8', 'user8@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (10, 'user9', 'user9@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (11, 'user10', 'user10@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (12, 'user11', 'user11@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (13, 'user12', 'user12@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (14, 'user13', 'user13@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (15, 'user14', 'user14@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (16, 'user15', 'user15@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (17, 'user16', 'user16@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (18, 'user17', 'user17@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (19, 'user18', 'user18@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (20, 'user19', 'user19@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (21, 'user20', 'user20@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (22, 'user21', 'user21@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (23, 'user22', 'user22@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (24, 'user23', 'user23@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (25, 'user24', 'user24@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (26, 'user25', 'user25@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (27, 'user26', 'user26@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (28, 'user27', 'user27@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (29, 'user28', 'user28@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (30, 'user29', 'user29@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (31, 'user30', 'user30@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (32, 'user31', 'user31@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (33, 'user32', 'user32@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (34, 'user33', 'user33@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (35, 'user34', 'user34@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (36, 'user35', 'user35@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (37, 'user36', 'user36@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (38, 'user37', 'user37@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (39, 'user38', 'user38@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (40, 'user39', 'user39@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+insert into users values (41, 'user40', 'user40@exsample.com', '$2b$12$BjhcKJo4Pkp9RrdlDVXZT.kFveZZP5ocA1jyNiiKiacmf0NcQAMhW', 1, 1);
+
+
+
+
 insert into users_genres values (1, 1, '日本食');
 insert into users_genres values (2, 1, '洋食');
 insert into users_genres values (3, 1, '中華');
@@ -6,10 +51,82 @@ insert into foods values (1, 1, '寿司', './food_img/sushi.jpg', 10, 5, 20, 5, 
 insert into foods values (2, 1, '天ぷら', './food_img/tenpura.jpg', 30, 50, 20, 2, 2);
 insert into foods values (3, 1, 'すき焼き', './food_img/sukiyaki.jpg', 50, 20, 20, 10, 5);
 insert into foods values (4, 1, 'ラーメン', './food_img/ramen.jpg', 10, 40, 60, 10, 0);
+
 insert into foods values (5, 2, 'ハンバーガー', './food_img/hamburger.jpg', 30, 20, 25, 30, 15);
 insert into foods values (6, 2, 'ステーキ', './food_img/steak.jpg', 11, 73, 84, 100, 57);
 insert into foods values (7, 2, 'パスタ', './food_img/spaghetti.jpg', 22, 24, 13, 53, 34);
 insert into foods values (8, 2, 'ピザ', './food_img/pizza.jpg', 3, 58, 82, 27, 47);
+
 insert into foods values (9, 3, '麻婆豆腐', './food_img/mabodoufu.jpg', 94, 95, 56, 11, 44);
 insert into foods values (10, 3, '炒飯', './food_img/chahan.jpg', 75, 9, 24, 64, 70);
 insert into foods values (11, 3, '肉まん', './food_img/nikuman.jpg', 44, 12, 84, 12, 36);
+
+insert into foods values (12, 1, 'お茶漬け', './food_img/ochaduke.jpg', 82, 10, 74, 43, 26);
+insert into foods values (13, 1, '湯豆腐', './food_img/yudouf.jpg', 30, 59, 98, 72, 11);
+insert into foods values (14, 1, '味噌汁', './food_img/misoshiru.jpg', 44, 73, 8, 20, 96);
+insert into foods values (15, 1, 'うどん', './food_img/udon.jpg', 21, 26, 36, 71, 79);
+insert into foods values (16, 1, 'そば', './food_img/soba.jpg', 93, 90, 40, 13, 8;
+insert into foods values (17, 1, 'お好み焼き', './food_img/okonomiyaki.jpg', 22, 49, 38, 7, 94);
+insert into foods values (18, 1, 'たこ焼き', './food_img/takoyaki.jpg', 38, 35, 65, 1, 81);
+insert into foods values (19, 1, 'とんかつ', './food_img/tonkatsu.jpg', 35, 33, 3, 97, 24);
+insert into foods values (20, 1, 'うな重', './food_img/unajyu.jpg', 9, 70, 74, 52, 34);
+
+insert into foods values (21, 2, 'オムライス', './food_img/omurice.jpg', 59, 36, 17, 30, 51);
+insert into foods values (22, 2, 'カレーライス', './food_img/curry.jpg', 43, 23, 56, 46, 9);
+insert into foods values (23, 2, 'グラタン', './food_img/guratan.jpg', 87, 94, 45, 65, 74);
+insert into foods values (24, 2, 'ポテトサラダ', './food_img/potato_sald.jpg', 11, 35, 22, 20, 4);
+insert into foods values (25, 2, 'お子様ランチ', './food_img/okosama_lunch.jpg', 97, 76, 74, 27, 48);
+insert into foods values (26, 2, 'ハヤシライス', './food_img/hayashi_rice.jpg', 21, 28, 38, 60, 87);
+insert into foods values (27, 2, 'エビフライ', './food_img/food_ebifry.jpg', 20, 68, 40, 30, 81);
+insert into foods values (28, 2, 'サンドイッチ', './food_img/sandwich_blt.jpg', 96, 47, 99, 39, 14);
+insert into foods values (29, 2, 'コーンスープ', './food_img/soup_corn.jpg', 20, 8, 91, 95, 88);
+insert into foods values (30, 2, 'シチュー', './food_img/food_creamstew.jpg', 87, 49, 72, 6, 55);
+
+
+
+
+
+insert into scores values (1, 2, 25, '2022-09-09');
+insert into scores values (2, 3, 36, '2022-09-09');
+insert into scores values (3, 4, 44, '2022-09-09');
+insert into scores values (4, 5, 23, '2022-09-09');
+insert into scores values (5, 6, 48, '2022-09-09');
+insert into scores values (6, 7, 9, '2022-09-09');
+insert into scores values (7, 8, 40, '2022-09-09');
+insert into scores values (8, 9, 14, '2022-09-09');
+insert into scores values (9, 10, 44, '2022-09-09');
+insert into scores values (10, 11, 6, '2022-09-09');
+insert into scores values (11, 12, 16, '2022-09-09');
+insert into scores values (12, 13, 45, '2022-09-09');
+insert into scores values (13, 14, 6, '2022-09-09');
+insert into scores values (14, 15, 9, '2022-09-09');
+insert into scores values (15, 16, 19, '2022-09-09');
+insert into scores values (16, 17, 6, '2022-09-09');
+insert into scores values (17, 18, 28, '2022-09-09');
+insert into scores values (18, 19, 17, '2022-09-09');
+insert into scores values (19, 20, 12, '2022-09-09');
+insert into scores values (20, 21, 20, '2022-09-09');
+insert into scores values (21, 22, 3, '2022-09-09');
+insert into scores values (22, 23, 26, '2022-09-09');
+insert into scores values (23, 24, 7, '2022-09-09');
+insert into scores values (24, 25, 44, '2022-09-09');
+insert into scores values (25, 26, 25, '2022-09-09');
+insert into scores values (26, 27, 47, '2022-09-09');
+insert into scores values (27, 28, 13, '2022-09-09');
+insert into scores values (28, 29, 15, '2022-09-09');
+insert into scores values (29, 30, 30, '2022-09-09');
+insert into scores values (30, 31, 36, '2022-09-09');
+insert into scores values (31, 32, 27, '2022-09-09');
+insert into scores values (32, 33, 32, '2022-09-09');
+insert into scores values (33, 34, 25, '2022-09-09');
+insert into scores values (34, 35, 34, '2022-09-09');
+insert into scores values (35, 36, 4, '2022-09-09');
+insert into scores values (36, 37, 1, '2022-09-09');
+insert into scores values (37, 38, 45, '2022-09-09');
+insert into scores values (38, 39, 3, '2022-09-09');
+insert into scores values (39, 40, 10, '2022-09-09');
+insert into scores values (40, 41, 47, '2022-09-09');
+insert into scores values (41, 2, 21, '2022-09-08');
+insert into scores values (42, 2, 32, '2022-09-07');
+insert into scores values (43, 2, 2, '2022-09-06');
+

76d6990e967db591a448bc6e057ad72ee140295b
kanno-takumi <bp20065@shibaura-it.ac.jp>
2022-09-09 10:55:08 +0900

    sidebar???

---
+++ frontend/app/src/components/SideBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <article>
+<div class="side">
+    <p>サイドバー</p>
+</div>
+
+<div class="content">
+    <p>メインコンテンツ</p>
+</div>
+</article>
+
+</template>
+
+<style scoped>
+    body{
+        font-family:'Noto Sans JP',sans-serif;
+        background-color:#f6f5f4;
+    }
+    .side{
+        background-color:#ff838b;
+        width:100px;
+        height:300px;
+        text-align:center;
+    }
+
+    .coontent{
+        background-color:#b3d874;
+        width:250px;
+        height:300px;
+        text-align:center;
+        margin-left:10px;
+    }
+
+    article{
+        display:flex;
+    }
+</style>
